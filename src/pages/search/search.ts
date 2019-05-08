a53c9eebb2840dfa2a49a13eee6ed588b7ea8d5f
Andre Morganti <acmorganti@ouitlook.com.br>
2019-05-08 09:49:20 -0300

    busca

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SearchProvider } from '../../providers/search/search';
+import { User } from '../../models/user';
+import { Post } from '../../models/post';
 
-/**
- * Generated class for the SearchPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +12,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SearchPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  currentItem = "users";
+  users: User[] = [];
+  posts: Post[] = [];
+
+  constructor(private nav: NavController, private searchProvider: SearchProvider){}
+
+  search(ev: any) {
+    this.searchProvider.search(ev.target.value).then(response => {
+      this.users = response.users;
+      this.posts = response.posts;
+    }, () => {});
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchPage');
+
+  goToUserPage(user) {
+    this.nav.push('user-page', { id: user.id })
+  }
+
+
+  writeDescriptionWithHashtags(description) {
+    return description.replace(new RegExp(/#\w+/, "gi"), match => {
+      return '<b>' + match + '</b>';
+    });
   }
 
 }
